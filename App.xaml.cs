ae61486a516ba290c49b58831030572f541e25ce
Karcub <rokaszemumenyecske@gmail.com>
2021-05-25 12:33:29 +0200

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace c_sharp_process_note
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace c_sharp_process_note
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
