415baad3b33b0dc1d6de9dfcb4469772a154c10a
Karcub <rokaszemumenyecske@gmail.com>
2021-05-25 14:41:40 +0200

    Feature: add/save comment

---
+++ Comment.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using c_sharp_process_note;
+
+namespace ProcessNote
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class commentsDialog : Window
+    {
+        public commentsDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+
+            var window2 = Application.Current.Windows
+                .Cast<Window>()
+                .FirstOrDefault(window => window is MainWindow) as MainWindow;
+
+            DataGridRow dgr = window2.ProcessInfo.ItemContainerGenerator.ContainerFromItem(window2.ProcessInfo.SelectedItem) as DataGridRow;
+            ListedProcess process = dgr.Item as ListedProcess;
+
+            process.Comments.Add(commentsBox.Text);
+            window2.commentsList.ItemsSource = " ";
+            window2.commentsList.ItemsSource = process.Comments;
+            this.Close();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,31 +1,79 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.Windows;
+using System.Windows.Controls;
+using ProcessNote;
 
 namespace c_sharp_process_note
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow
+    public partial class MainWindow : Window
     {
         public Process[] processes;
-        public string[] processesss;
+        List<ListedProcess> processlist = new List<ListedProcess>();
+        
         public MainWindow()
         {
             InitializeComponent();
             processes = Process.GetProcesses();
-            List<processlist> processlist = new List<processlist>();
             foreach (Process item in processes)
             {
-                processlist.Add(new processlist() { id = item.Id, name = item.ProcessName });
+                processlist.Add(new ListedProcess() { id = item.Id, name = item.ProcessName });
+                
             }
             ProcessInfo.ItemsSource = processlist;
         }
+        
+        private void dataGrid_selectedRow(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender != null)
+            {
+                DataGrid grid = sender as DataGrid;
+                refreshData();
+            }
+        }
+        
+        private void refreshData()
+        {
+            if (ProcessInfo.SelectedItems != null && ProcessInfo.SelectedItems.Count == 1)
+            {
+                DataGridRow dgr = ProcessInfo.ItemContainerGenerator.ContainerFromItem(ProcessInfo.SelectedItem) as DataGridRow;
+                ListedProcess process = dgr.Item as ListedProcess;
+                
+                commentsList.ItemsSource = process.Comments;
+            }
+        }
+
+        private void AddComment_Click(object sender, RoutedEventArgs e)
+        {
+            if (ProcessInfo.SelectedItems != null && ProcessInfo.SelectedItems.Count == 1)
+            {
+                var dialog = new commentsDialog();
+                dialog.ShowDialog();
+            }
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            if (ProcessInfo.SelectedItems != null && ProcessInfo.SelectedItems.Count == 1)
+            {
+                DataGridRow dgr = ProcessInfo.ItemContainerGenerator.ContainerFromItem(ProcessInfo.SelectedItem) as DataGridRow;
+                ListedProcess process = dgr.Item as ListedProcess;
+                Process.Start("http://google.com/search?q="+process.name);
+            }
+            else
+            {
+                Process.Start("https://github.com/CodecoolGlobal/c-sharp-process-note-c-_processnote");
+            }
+        }
     }
-    public class processlist
+    public class ListedProcess
     {
         public int id { get; set; }
         public string name { get; set; }
+        
+        public List<string> Comments = new List<string>();
     }
 }
