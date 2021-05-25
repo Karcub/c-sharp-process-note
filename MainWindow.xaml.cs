df5c74252ca3a4c4bb369b6d136f0e9cb4ec3b56
Horváth Dániel <61006437+DonRicardoGosta@users.noreply.github.com>
2021-05-25 13:17:32 +0200

    MainWindow - processes has listed and got a basic design

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,31 @@
-﻿namespace c_sharp_process_note
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows;
+
+namespace c_sharp_process_note
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow
     {
+        public Process[] processes;
+        public string[] processesss;
         public MainWindow()
         {
             InitializeComponent();
+            processes = Process.GetProcesses();
+            List<processlist> processlist = new List<processlist>();
+            foreach (Process item in processes)
+            {
+                processlist.Add(new processlist() { id = item.Id, name = item.ProcessName });
+            }
+            ProcessInfo.ItemsSource = processlist;
         }
+    }
+    public class processlist
+    {
+        public int id { get; set; }
+        public string name { get; set; }
     }
 }
